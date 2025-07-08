d873ad6f28f5011639f9dfe5371db160979b1c92
Martin Vargas <martin.vargas@udla.edu.ec>
2025-07-07 19:45:16 -0500

    style: Apply definitive auto-formatting with Black

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,16 +4,11 @@ import pytest
 from fastapi.testclient import TestClient
 from typing import Generator
 
-# Importamos la app principal de FastAPI para que el TestClient pueda interactuar con ella.
 from main import app
 
 
 @pytest.fixture(scope="module")
 def client() -> Generator:
-    """
-    Crea un cliente de prueba para la API.
-
-    El scope="module" lo crea una vez por archivo, haciéndolo eficiente.
-    """
+    """Crea un cliente de prueba para la API."""
     with TestClient(app) as c:
         yield c
