92fb673ee7b7f5be8c8762f2432d5013c922d694
Martin Vargas <martin.vargas@udla.edu.ec>
2025-07-07 20:26:40 -0500

    fix(tests): Correct auth test and increase coverage

--- api/security.py
+++ api/security.py
@@ -29,5 +29,5 @@ def get_api_key(api_key: str = Security(api_key_header)):
     else:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="API Key inválida o ausente.",
+            detail="Not authenticated.",
         )
